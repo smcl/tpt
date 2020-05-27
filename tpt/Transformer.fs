e59ab10ca4207715dc50e29a7c9cb35bd14b2963
Sean McLemon <sean.mclemon@gmail.com>
2020-05-27 23:44:07 +0100

    Minor tidy-up of Transformer

--- tpt/Transformer.fs
+++ tpt/Transformer.fs
@@ -5,14 +5,6 @@ open System.IO
 open System.Xml
 open Microsoft.Web.XmlTransform
 
-let openSource (squashWhitespace:bool) (source:string) = 
-    let document = new XmlDocument(PreserveWhitespace = not squashWhitespace)
-    document.Load(source)
-    document
-
-let openTransform (transform:string) = 
-    new XmlTransformation(transform)
-
 let tryOpen openFunc xmlFile = 
     try
         openFunc xmlFile
@@ -21,16 +13,34 @@ let tryOpen openFunc xmlFile =
         | :? XmlException as ex -> Failure (sprintf "Unable to parse %s, check it is valid XML" xmlFile)
         | :? FileNotFoundException as ex -> Failure (sprintf "Unable to open %s, check that the file exists and that you have permission to access it" xmlFile)
         | _ -> Failure (sprintf "Something went wrong when loading the XML: %s" xmlFile)
+
+let getDocument source squashWhitespace =
+    let openSource (squashWhitespace:bool) (source:string) =
+        let document = new XmlDocument(PreserveWhitespace = not squashWhitespace)
+        document.Load(source)
+        document
+    tryOpen
+        (openSource squashWhitespace)
+        source
+
+let getTransformation transform =
+    let openTransformFile path = new XmlTransformation(path)
+    tryOpen
+        openTransformFile
+        transform
+
+let applyTransform (x:XmlTransformation) (d:XmlDocument) =
+    if x.Apply(d)
+    then Success d
+    else Failure "Error applying transform"
     
-let apply (source:string) (transform:string) (squashWhitespace: bool) = 
-    let document = (openSource squashWhitespace |> tryOpen) source
-    let transformation = (openTransform |> tryOpen) transform
-    (* TODO: this match->match->if->else is hideous *)
-    match document with
-    | Failure e -> Failure e
-    | Success d -> 
-        match transformation with
-        | Failure e -> Failure e
-        | Success t -> if t.Apply(d)
-                       then Success d.OuterXml
-                       else Failure "Error applying transformation"
+let apply (source:string) (transform:string) (squashWhitespace: bool) =
+    let document = getDocument source squashWhitespace
+    let transformation = getTransformation transform
+    match document, transformation with
+    | Success d, Success t ->
+        applyTransform t d
+        >>= (fun (q:XmlDocument) -> Success q.OuterXml)
+    | Success _, Failure e -> Failure e
+    | Failure e, Failure _ -> Failure e
+    | Failure e, Success _ -> Failure e
