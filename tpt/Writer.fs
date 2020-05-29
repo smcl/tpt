c8eda3f3e1f224914c9cc739edea90f53aef737b
Sean McLemon <sean.mclemon@gmail.com>
2020-05-29 19:49:47 +0100

    Handle missing dir, or unauthorized access to output dir

--- tpt/Writer.fs
+++ tpt/Writer.fs
@@ -2,15 +2,24 @@
 
 open Common
 open System.IO
+open System
 
 let writeToFile destination result =
-    File.WriteAllText(destination, result)
+    try 
+        File.WriteAllText(destination, result) |> Success
+    with 
+        | :? DirectoryNotFoundException as ex -> Failure (sprintf "Couldn't write output to %s - check the directory exists" destination)
+        | :? UnauthorizedAccessException as ex -> Failure (sprintf "Couldn't write output to %s - check that you have permission to write to it" destination)
+        | _ -> Failure (sprintf "Something went wrong writing to %s" destination)
 
-let getWriter destination =
-    match destination with 
+let writeToConsole result =
+    printfn "%s" result |> Success
+
+let getWriter = function
     | Some dest -> writeToFile dest
-    | None -> printfn "%s"
+    | None -> writeToConsole
 
 let write (destination:string option) (output:string) = 
-    output |> getWriter destination |> Success
+    let writer = getWriter destination
+    writer output
     
